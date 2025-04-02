28f7cbcbcf18f3fe673598caa43aa58fe089a28f
Andi Maliqi <144494359+Maliqi06@users.noreply.github.com>
2025-04-03 01:15:56 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,13 +1,3 @@
-/*
-===============================================================================
-DDL Script: Create Bronze Tables
-===============================================================================
-Script Purpose:
-    This script creates tables in the 'bronze' schema, dropping existing tables 
-    if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
-*/
 
 IF OBJECT_ID('bronze.source_address', 'U') IS NOT NULL
     DROP TABLE bronze.source_address;
@@ -15,8 +5,8 @@ GO
 
 CREATE TABLE bronze.source_address (
     address_id          INT,
-    address_Line_1      NVARCHAR(100),
-    address_Line_2      NVARCHAR(100),
+    Address_Line_1      NVARCHAR(100),
+    Address_Line_2      NVARCHAR(100),
     city                NVARCHAR(50),
     state_province      NVARCHAR(50),
     country_region      NVARCHAR(50),
@@ -62,11 +52,11 @@ CREATE TABLE bronze.source_customer_address (
 );
 GO
 
-IF OBJECT_ID('bronze.source_customer_product', 'U') IS NOT NULL
-    DROP TABLE bronze.source_customer_product;
+IF OBJECT_ID('bronze.source_product', 'U') IS NOT NULL
+    DROP TABLE bronze.source_product;
 GO
 
-CREATE TABLE bronze.source_customer_product (
+CREATE TABLE bronze.source_product (
     product_id                  INT,
     name_info                   NVARCHAR(100),
     product_number              NVARCHAR(50),
@@ -74,13 +64,12 @@ CREATE TABLE bronze.source_customer_product (
     standard_cost               DECIMAL(10, 6),
     list_price                  DECIMAL(10, 6),
     size                        NVARCHAR(50),
-    weight_info                 DECIMAL(10, 6),
-    product_category_id         INT,
-    product_model_id            INT,
+    weight_info                 NVARCHAR(50),
+    product_category_id         NVARCHAR(50),
+    product_model_id            NVARCHAR(50),
     sell_start_date             DATETIME,
-    sell_end_date               DATETIME,
-    discontinued_date           DATETIME,
-    thumbnail_photo             VARBINARY(MAX),
+    sell_end_date               NVARCHAR(50),
+    discontinued_date           NVARCHAR(50),
     thumbnail_photo_file_name   NVARCHAR(100),
     row_guid                    NVARCHAR(100),
     modified_date               DATETIME
@@ -136,3 +125,4 @@ CREATE TABLE bronze.source_product_model_product_description (
     modified_date            DATETIME
 );
 GO
+
